08a842104be13116c9d4c32174076e91aa557ab3
cesine <cesine@yahoo.com>
2017-03-19 13:55:00 -0400

    added a dependancy
    to demo that the tests cant access their own deps
    if run in workers without the cwd set

--- test/in-component-b.js
+++ test/in-component-b.js
@@ -1,4 +1,5 @@
 var config = require('config');
+var diacritics = require('diacritics');
 
 module.exports = {
   'should support unicode urls': function(client) {
@@ -6,6 +7,15 @@ module.exports = {
     client
       .url('https://' + config.componentB.wikipedia.locale + '.m.wikipedia.org/wiki/' + config.componentB.wikipedia.article)
       .assert.urlEquals('https://ka.m.wikipedia.org/wiki/%E1%83%93%E1%83%94%E1%83%93%E1%83%90_%E1%83%94%E1%83%9C%E1%83%90')
+      .end();
+  },
+
+  'should be able to use its own node_modules': function(client) {
+    var topic = diacritics.remove("Iлｔèｒｎåｔïｏｎɑｌíƶａｔïoԉ");
+
+    client
+      .url('https://en.m.wikipedia.org/wiki/' + topic)
+      .assert.urlEquals('https://en.m.wikipedia.org/wiki/Internationalization')
       .end();
   }
 };
