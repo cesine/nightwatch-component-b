599b5f5b246b093280391eda48b128ba540b3cb0
cesine <cesine@yahoo.com>
2017-03-19 12:36:44 -0400

    namespacing config

--- test/in-component-b.js
+++ test/in-component-b.js
@@ -2,9 +2,9 @@ var config = require('config');
 
 module.exports = {
   'should support unicode urls': function(client) {
-    console.log('using config', config);
+    console.log('Component B using config', config);
     client
-      .url('https://' + config.wikipedia.locale + '.m.wikipedia.org/wiki/' + config.wikipedia.article)
+      .url('https://' + config.componentB.wikipedia.locale + '.m.wikipedia.org/wiki/' + config.componentB.wikipedia.article)
       .assert.urlEquals('https://ka.m.wikipedia.org/wiki/%E1%83%93%E1%83%94%E1%83%93%E1%83%90_%E1%83%94%E1%83%9C%E1%83%90')
       .end();
   }
